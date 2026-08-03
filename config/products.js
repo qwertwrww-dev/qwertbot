613d2f08581686f58a257309640b52f215e0f86d
qwertwrww-dev <qwertwrww@gmail.com>
2026-08-03 17:02:39 +0700

    Create products.js

---
+++ config/products.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+  ToolsBot: {
+    id: "ToolsBot",
+    name: "🤖 ToolsBot",
+    price: 100,
+    currency: "usd",
+    payCurrency: "ton",
+    description: "Premium ToolsBot Access"
+  },
+
+  TRACK: {
+    id: "TRACK",
+    name: "📍Track Mobile Number",
+    price: 20,
+    currency: "usd",
+    payCurrency: "ton",
+    description: "Track Mobile Number In Real-time"
+  },
+
+  DOXXING: {
+    id: "DOXXING",
+    name: "😈 DOXXING",
+    price: 30,
+    currency: "usd",
+    payCurrency: "ton",
+    description: "Doxxing The Target's Identity"
+  }
+
+};
