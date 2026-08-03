bd26f43a683d9820f953a535d7ec2b345e1a9910
qwertwrww-dev <qwertwrww@gmail.com>
2026-08-03 20:07:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ bot.onText(/\/start/, async (msg) => {
 
   await bot.sendMessage(
     msg.chat.id,
-    "🌍 WELLCOME!\n\n!QWERT is a team founded in 2022\n\n!Here you can order tracking and doxxing services\n\n!Please select an option below to order.",
+    "🌍 WELLCOME!\n\nQWERT IS A TEAM FOUNDED IN 2022\nHERE YOU CAN ORDER TRACKING AND DOXXING SERVICES\n\nPlease select an option below to order.",
     {
       reply_markup: {
         inline_keyboard: buttons
